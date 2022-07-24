f91947f00c94de594fdd05b1d35a80cbb2fd8d9d
Youngjae Choi <cdw0807@naver.com>
2022-07-24 23:53:16 +0900

    enum case

---
+++ Grammer.playground/Pages/enumCase.xcplaygroundpage/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+import Foundation
+
+//enum School {
+//    case elementary
+//    case middle
+//    case high
+//}
+
+// , 로 case 생략가능
+enum School {
+    case elementry, middle, high
+}
+
+let yourSchool = School.high
+print("yourSchool: \(yourSchool)")
+
+
+// 값(first, second 등) 명시 가능
+enum Grade: Int {
+    case first = 1
+    case second = 2
+}
+
+let yourGrade = Grade.second
+print("yourGrade: \(yourGrade)")
+
+// rawValue : case의 값에 접근
+print("yourGrade: \(yourGrade.rawValue)")
+
+
+enum SchoolDetail {
+    case elementary(name: String)
+    case middle(name: String)
+    case high(name: String)
+    
+    func getName() -> String {
+        switch self {
+        case .elementary(let name):
+            return name
+        case .middle(let name):
+            return name
+        case .high(let name):
+            return name
+        }
+    }
+}
+
+let yourMiddleSchoolName = SchoolDetail.middle(name: "iOS 중학교")
+print("yourMiddleSchoolName: \(yourMiddleSchoolName)")
+print("yourMiddleSchoolName: \(yourMiddleSchoolName.getName())")
